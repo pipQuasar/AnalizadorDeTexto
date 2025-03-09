11d39c4664c6c97d11322766d4529d33866587ee
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2025-03-09 17:31:01 -0300

    Algunas funciones nuevas!

--- AnalizadorDeTexto_cod.cpp
+++ AnalizadorDeTexto_cod.cpp
@@ -3,14 +3,14 @@
     para ir empezando y cumplirlos para que el Analizador posea funcionalidad.
 
   Introducir un texto aleatorio y:
-  a) Identificar cuál es la palabra que mas se repite y cuántas veces.
-  b) Cantidad de veces que aparezca la letra 'y' y en qué posición aparecieron.
-  c) Mostrar la cantidad de veces que aparecieron comas ','. 
-  d) ¿Cuántos caracteres hay en total?
-  e) ¿Cuántas veces se usó espacio ' '?
-  f) Informar la cantidad de mayúsculas existentes
+  a) Cantidad de veces que aparezca la letra 'y' y en qué posición aparecieron.
+  b) Mostrar la cantidad de veces que aparecieron comas ','. 
+  c) ¿Cuántos caracteres hay en total?
+  d) ¿Cuántas veces se usó espacio ' '?
+  e) Informar la cantidad de mayúsculas existentes
 */
 #include <iostream>
+#include <cctype>
 using namespace std;
 
 void obtenerOracion(char oracion[], int tamanio) {
@@ -19,15 +19,43 @@ void obtenerOracion(char oracion[], int tamanio) {
 }
 
 int main () {
- char oracion[INT32_MAX];
- obtenerOracion(oracion, INT32_MAX);
+ char oracion[999999];
+ obtenerOracion(oracion, 999999);
  // a)
+  int contador_y = 0;
+  int posiciones_y[100];
+ // b)
+  int contador_comas = 0;
+ // c)
+  int contador_caracteres = 0;
+ // d)
+  int contador_espacios = 0;
+ // e)
+  int cantidad_mayusculas = 0;
 
-
- for (int c = 0; oracion[c] != ' '; c++)
- {
-  
+ for (int c = 0; oracion[c] != '\0'; c++)
+  {  
+  if (tolower(oracion[c]) == 'y'){
+    posiciones_y[contador_y] = c;
+    contador_y++; 
+  }
+  if (isupper(oracion[c])) cantidad_mayusculas++; 
+  if (oracion[c] == ',') contador_comas++;
+  if (oracion[c] == ' ') contador_espacios++;
+  contador_caracteres++;
  }
  
+
+ cout << "Cantidad de 'y': " << contador_y << "\nY aparece en los caracteres: "; // a)
+ for (int i = 0; i < contador_y; i++)
+ {
+  cout << posiciones_y[i] << "\t";
+ }
+ cout << "\n";
+
+ cout << "Hubo un total de " << contador_comas << " comas\n"; // b)
+ cout << "Hay un total de " << contador_caracteres << " caracteres\n"; // c)
+ cout << "Aparecieron un total de " << contador_espacios << " espacios\n"; // d)
+ cout << "Total de mayusculas: " << cantidad_mayusculas << "\n"; // e)
  return 0;
 }
