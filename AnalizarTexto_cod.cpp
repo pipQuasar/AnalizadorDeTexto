77d12a54f398ff66220c45cd7da6227ba6d6bf1d
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2025-03-08 22:58:47 -0300

    Create AanalizarTexto_cod

---
+++ AnalizarTexto_cod.cpp
@@ -0,0 +1,31 @@
+/*
+  Introducir un texto aleatorio y:
+  a) Identificar cuál es la palabra que mas se repite y cuántas veces.
+  b) Cantidad de veces que aparezca la letra 'y' y en qué posición aparecieron.
+  c) Mostrar la cantidad de veces que aparecieron comas ',' y puntos '.'. 
+  d) ¿Cuántos caracteres hay en total?
+  e) ¿Cuántas veces se usó espacio ' '?
+  f) Informar la cantidad de mayúsculas existentes
+  g) ¿Cuál fué la palabra mas corta? (exeptuando las 'y' y las ',')
+*/
+#include <iostream>
+using namespace std;
+
+void obtenerOracion(char oracion[], int tamanio) {
+  cout << "Ingrese una oracion terminada en punto: ";
+  cin.getline(oracion, tamanio);
+}
+
+int main () {
+ char oracion[INT32_MAX];
+ obtenerOracion(oracion, INT32_MAX);
+ // a)
+ 
+
+ for (int c = 0; oracion[c] != ' '; c++)
+ {
+  
+ }
+ 
+ return 0;
+}
